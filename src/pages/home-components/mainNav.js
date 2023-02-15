6f8a074eddd55ef961bc22c2701a3e04aa6d6f2c
J Wood <73401121+DFK333@users.noreply.github.com>
2023-02-14 21:29:11 -0600

    intial setup

---
+++ src/pages/home-components/mainNav.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+
+export default function MainNav() {
+  return (
+    <>
+      <nav className={styles.mainNav}>
+        {/* <link href="/About">About</link> */}
+      </nav>
+    </>
+  );
+}
