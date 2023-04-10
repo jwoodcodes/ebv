04097cc25abefc197625b186afebccb1ba4bc754
J Wood <73401121+DFK333@users.noreply.github.com>
2023-04-10 18:04:57 -0500

    added shop data

--- src/shopData.js
+++ src/shopData.js
@@ -1,6 +1,105 @@
-const allShopData = {
-  juice: {
-    name: "Elderberry Juice",
-    type: "Product",
-  },
+export default allShopData = {
+  shopData: [
+    {
+      title: "Elderberry Juice",
+      type: "Product",
+      price: "$20.00",
+      imgsrc: "/juice.webp",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of elderberry juice",
+      description:
+        "Undiluted and unsweetened, our juice is pressed and blended from multiple elderberry varieties grown at our orchard. Take a tablespoon daily, or try adding to a shake, smoothie, yogurt, fruit juice or ice cream.",
+    },
+
+    {
+      title: "Elderflower Nectar",
+      type: "Product",
+      price: "$20.00",
+      imgsrc: "/elderflower-necter.webp",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of elderflower necter",
+      description:
+        "Hand harvested elderflowers, lemon juice, and cane sugar combine for a syrup to sweeten teas or brighten a glass of champagne. A flavorful dessert ingredient and cocktail mixer.  Add with juice to sparkling water to create an elderberry lemonade.",
+    },
+
+    {
+      title: "Elderberry Relief",
+      type: "Product",
+      price: "$20.00",
+      imgsrc: "/relief.webp",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of elderberry relief",
+      description:
+        "Our pure elderberry juice with added honey and extracts of Ceylon cinnamon, clove and elderflower. Take a tablespoon daily, or as often as desired. Especially soothing for a sore throat or easing a cough.",
+    },
+    {
+      title: "Elderberry Relief With Ginger",
+      type: "Product",
+      price: "$20.00",
+      imgsrc: "/relief-with-ginger.webp",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of elderberry relief with ginger",
+      description:
+        "Our pure elderberry juice with added honey and extracts of Ceylon cinnamon, clove, elderflower, and ginger. Take a tablespoon daily, or as often as desired. Especially soothing for a sore throat or to ease a cough.",
+    },
+    {
+      title: "Elderberry Wine Vinegar",
+      type: "Product",
+      price: "$30.00",
+      imgsrc: "/elderberry-wine-vinegar.webp",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of elderberry wine vinegar",
+      description:
+        "Our pure elderberry wine oak aged with Mother of vinegar over three years to make a unique full bodied wine vinegar. Use it like a Balsamic for salads, marinades, or sprinkled over a plate of olive oil for dipping bread",
+    },
+    {
+      title: "Dry Elderberry Wine",
+      type: "Product",
+      price: "$40.00",
+      imgsrc: "/dry-wine.jpg",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of dry elderberry wine lderberry wine vinegar",
+      description:
+        "Full bodied, deep garnet color, dry elderberry wine with a vanilla spice, oaky, wild berries/cherries aroma. 2019 Finger Lakes International Wine Competition Bronze Medal Winner. 2019 New York International Wine Competition Silver Medal Winner. 2019 USA Wine Ratings - San Francisco Silver Medal Winner",
+    },
+    {
+      title: "Semi-Dry Elderberry Wine",
+      type: "Product",
+      price: "$40.00",
+      imgsrc: "/semi-dry-wine.jpg",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of semi-dry elderberry wine lderberry wine vinegar",
+      description:
+        "Full bodied, deep garnet color, semi-dry elderberry wine with a vanilla spice, oaky, wild berries/cherries aroma. 2019 Finger Lakes International Wine Competition Bronze Medal Winner. 2019 New York International Wine Competition Silver Medal Winner. USA Wine Ratings - San Francisco Silver Medal Winner",
+    },
+    {
+      title: "Semi-Sweet Elderberry Wine",
+      type: "Product",
+      price: "$40.00",
+      imgsrc: "/semi-sweet-wine.jpg",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of semi-sweet elderberry wine lderberry wine vinegar",
+      description:
+        "Full bodied, deep garnet color, semi-sweet elderberry wine with a vanilla spice, oaky, wild berries/cherries aroma",
+    },
+    {
+      title: "Sweet Elderberry Wine",
+      type: "Product",
+      price: "$40.00",
+      imgsrc: "/sweet-wine.jpg",
+      imgWidth: "360",
+      imgHeight: "440",
+      altText: "Bottle of sweet elderberry wine lderberry wine vinegar",
+      description:
+        "Full bodied, deep garnet color, sweet elderberry wine with a vanilla spice, oaky, wild berries/cherries aroma. Excellent as a chilled desert wine.",
+    },
+  ],
 };
