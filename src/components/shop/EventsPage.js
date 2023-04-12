73c26cabe9fa6f93a4e39e596c9de3cefb6d2670
J Wood <73401121+DFK333@users.noreply.github.com>
2023-04-11 21:55:24 -0500

    s

--- src/components/shop/EventsPage.js
+++ src/components/shop/EventsPage.js
@@ -9,7 +9,7 @@ export default function EventsPage() {
         imageURL="/bel-airs.jpg"
         title="The Bel Airs"
         price="$5.00"
-        alttext="the band the Bel Airs"
+        altText="the band the Bel Airs"
         width="360"
         height="360"
       />
@@ -17,7 +17,7 @@ export default function EventsPage() {
         imageURL="/mick-byrd.jpg"
         title="Mick Byrd and the Backroad Band"
         price="$5.00"
-        alttext="Mick Beard"
+        altText="Mick Beard"
         width="360"
         height="360"
       />
@@ -25,7 +25,7 @@ export default function EventsPage() {
         imageURL="/hart-creek-ramblers.jpg"
         title="Hart Creek Ramblers"
         price="$5.00"
-        alttext="Hart Creek Ramblers Band"
+        altText="Hart Creek Ramblers Band"
         width="360"
         height="360"
       />
@@ -33,7 +33,7 @@ export default function EventsPage() {
         imageURL="/apostles-creek.jpg"
         title="Apostle's Creek"
         price="$5.00"
-        alttext="Apostle's Creek Band"
+        altText="Apostle's Creek Band"
         width="360"
         height="360"
       />
