b37f21af7fe520d5fc4f582db236c63106f6f5a9
J Wood <73401121+DFK333@users.noreply.github.com>
2023-04-16 20:48:44 -0500

    d

--- src/pages/api/dbHandler.js
+++ src/pages/api/dbHandler.js
@@ -1,18 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// import clientPromise from "@/lib/mongodb";
+import clientPromise from "@/lib/mongodb";
 
-// const dbHandler = async (req, res) => {
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db("shopData");
+const dbHandler = async (req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("shopData");
 
-//     const data = await db.collection("shopData").find({}).toArray();
-//     res.json(data);
-//     console.log(data);
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
+    const data = await db.collection("shopData").find({}).toArray();
+    res.json(data);
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-// export default dbHandler;
+export default dbHandler;
