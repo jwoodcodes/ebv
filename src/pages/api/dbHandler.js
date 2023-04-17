12108573d5993350ebba72a0eb0e0f09aaadfa47
J Wood <73401121+DFK333@users.noreply.github.com>
2023-04-16 20:00:53 -0500

    deploy fixes 2

--- src/pages/api/dbHandler.js
+++ src/pages/api/dbHandler.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import clientPromise from "lib/mongodb";
+import clientPromise from "@/lib/mongodb";
 
-export default async (req, res) => {
+const dbHandler = async (req, res) => {
   try {
     const client = await clientPromise;
     const db = client.db("shopData");
@@ -14,3 +14,5 @@ export default async (req, res) => {
     console.error(e);
   }
 };
+
+export default dbHandler;
