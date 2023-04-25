74c0d563936478d0c0dc532a62858b25f4028dff
J Wood <73401121+DFK333@users.noreply.github.com>
2023-04-24 21:06:30 -0500

    shop card UI finalized

--- src/components/shop/ItemCard.js
+++ src/components/shop/ItemCard.js
@@ -22,7 +22,16 @@ export default function ItemCard({
       ></Image>
       <div className={styles.cardItemTitle}>{title}</div>
       <div className={styles.cardItemPrice}>{price}</div>
-      <Link href="/Shop/[title]" className={styles.cardMoreDetails}>
+      // next thing to try is to put the Link below into a component and render
+      that component below and // pass ${title} as a prop to that component. IDK
+      <Link
+        href={{
+          pathname: "/ShopItemDetails/[title]",
+          query: { title: `${title}` },
+        }}
+        className={styles.cardMoreDetails}
+        title={`${title}`}
+      >
         Full Description & To Order &rarr;
       </Link>
     </div>
---
+++ src/components/shop/ItemDetailsPage.js
@@ -0,0 +1,7 @@
+export default function ItemDetailsPage() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
