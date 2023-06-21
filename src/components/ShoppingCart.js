6ae81a8385c361971fad683f5320024f5bd381f1
J Wood <73401121+DFK333@users.noreply.github.com>
2023-06-20 21:15:05 -0500

    convert cart to my way

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,10 +1,16 @@
 import { useShoppingCart } from "@/context/ShoppingCartContext";
+import MainNav from "./MainNav";
 
 export default function ShoppingCart(ShoppingCartProps) {
-  const { cartItems } = useShoppingCart;
+  const { cartItems, cartQuantity } = useShoppingCart;
   return (
     <div>
+      <MainNav />
       <div>cart</div>
+      {cartItems.map((items) => {
+        // console.log(items.data);
+        // console.log(cartQuantity);
+      })}
     </div>
   );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,36 @@
+import MainNav from "@/components/MainNav";
 import { useShoppingCart } from "@/context/ShoppingCartContext";
+import Image from "next/image";
+import styles from "@/styles/cart.module.css";
 
 export default function Cart(ShoppingCartProps) {
-  const { cartItems } = useShoppingCart();
+  const { cartItems, cartQuantity } = useShoppingCart();
   return (
     <div>
-      <div>cart</div>
-      {cartItems.map((items) => {
-        console.log(items.data);
+      <MainNav />
+      <div className={styles.cartTitle}>Cart Checkout</div>
+      {cartItems.map((item) => {
+        console.log(item.data);
+        console.log(cartQuantity);
+
+        return (
+          <div className={styles.cartItemWrapper}>
+            <Image
+              src={item.data.imgsrc}
+              width="250"
+              height="250"
+              priority
+              alt={item.data.alttext}
+              key={item.data.APIid}
+              className={styles.cartImg}
+            />
+            <div>{item.data.title}</div>
+            <div>{item.data.actualPrice}</div>
+            <div>
+              `Total Price: ${+item.data.actualPrice.slice(1) * cartQuantity}`
+            </div>
+          </div>
+        );
       })}
     </div>
   );
