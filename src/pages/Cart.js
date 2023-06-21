456122a22126ad6a4c141678b50e6fc9a3dca5cb
J Wood <73401121+DFK333@users.noreply.github.com>
2023-06-20 21:35:51 -0500

    d

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,33 +5,38 @@ import styles from "@/styles/cart.module.css";
 
 export default function Cart(ShoppingCartProps) {
   const { cartItems, cartQuantity } = useShoppingCart();
-  return (
-    <div>
-      <MainNav />
-      <div className={styles.cartTitle}>Cart Checkout</div>
-      {cartItems.map((item) => {
-        console.log(item.data);
-        console.log(cartQuantity);
+  if (cartItems) {
+    return (
+      <div>
+        <MainNav />
+        <div className={styles.cartTitle}>Cart Checkout</div>
+        {cartItems.map((item) => {
+          console.log(item.data);
+          console.log(cartQuantity);
 
-        return (
-          <div key={item.data.APIid} className={styles.cartItemWrapper}>
-            <Image
-              src={item.data.imgsrc}
-              width="250"
-              height="250"
-              priority
-              alt={item.data.alttext}
-              key={item.data.APIid}
-              className={styles.cartImg}
-            />
-            <div>{item.data.title}</div>
-            <div>{item.data.actualPrice}</div>
-            <div>
-              {/* `Total Price: ${+item.data.actualPrice.slice(1) * cartQuantity}` */}
+          return (
+            <div key={item.data.APIid} className={styles.cartItemWrapper}>
+              <Image
+                src={item.data.imgsrc}
+                width="250"
+                height="250"
+                priority
+                alt={item.data.alttext}
+                key={item.data.APIid}
+                className={styles.cartImg}
+              />
+              <div>{item.data.title}</div>
+              <div>{item.data.actualPrice}</div>
+              <div>
+                {/* `Total Price: ${+item.data.actualPrice.slice(1) * cartQuantity}` */}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
+  }
+  if (!cartItems) {
+    return <div>Cart is empty</div>;
+  }
 }
